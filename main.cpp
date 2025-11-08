b6be141a49b615e6ae399214065067f8cd800ba9
AxDevl <bartoszgodlewski7@gmail.com>
2025-11-08 12:12:33 +0000

    new

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <climits>
 #include <string>
 
+
 // ------- PIN references----------------
 
 TextLCD lcd(D14, D15, D13, D12, D11, D10, TextLCD::LCD20x4); // Connect these nucleo pins to RS, E, D4, D5, D6 and D7 pins of the LCD
@@ -42,42 +43,40 @@ PinName ReturnPinName(int count)
     return D10;
 }
 
-/*
-This code is to return letters to appriopriate LED format.
-*/
 
 string ReturnLetterString(char count)
 {
+    
     switch(count)
     {
-        case 'a': return 00001110;
-        case 'b': return 01010011;
-        case 'c': return 00001011;
-        case 'd': return 11000101;
-        case 'e': return 01001001;
-        case 'f': return 10001001;
-        case 'g': return 01110001;
-        case 'h': return 10010011;
-        case 'i': return 10100000;
-        case 'j': return 01110000;
-        case 'k': return 10000011;
-        case 'l': return 11000000;
-        case 'm': return 10011000;
-        case 'n': return 00010001;
-        case 'o': return 00001111;
-        case 'p': return 10001101;
-        case 'q': return 00011011;
-        case 'r': return 10000001;
-        case 's': return 01011001;
-        case 't': return 10001010;
-        case 'u': return 11010000;
-        case 'v': return 11110000;
-        case 'w': return 00100111;
-        case 'x': return 10010110;
-        case 'y': return 00110011;
-        case 'z': return 01101101;
+        case 'a': return "00001110";
+        case 'b': return "01010011";
+        case 'c': return "00001011";
+        case 'd': return "11000101";
+        case 'e': return "01001001";
+        case 'f': return "10001001";
+        case 'g': return "01110001";
+        case 'h': return "10010011";
+        case 'i': return "10100000";
+        case 'j': return "01110000";
+        case 'k': return "10000011";
+        case 'l': return "11000000";
+        case 'm': return "10011000";
+        case 'n': return "00010001";
+        case 'o': return "00001111";
+        case 'p': return "10001101";
+        case 'q': return "00011011";
+        case 'r': return "10000001";
+        case 's': return "01011001";
+        case 't': return "10001010";
+        case 'u': return "11010000";
+        case 'v': return "11110000";
+        case 'w': return "00100111";
+        case 'x': return "10010110";
+        case 'y': return "00110011";
+        case 'z': return "01101101";   
     }
-    return 0;
+    return "00000000";
 }
 
 /*
@@ -110,6 +109,7 @@ Parameter
 0 = Home Screen
 1 = First Name
 2 = Last Name
+
 */
 
 void executionLCD(int buttonSelection)
@@ -151,8 +151,8 @@ The main section will the first peice of code that is executed when the program 
 
 int main()
 {
-    string firstName = "Barto";
-    string LastName = "Godle";
+    string firstName = "barto";
+    string LastName = "godle";
 
     executionLCD(0);
    
@@ -163,8 +163,8 @@ int main()
                 executionLCD(1);
                 for (char c : firstName)
                 {
-                    char letterDisplay = ReturnLetterString(c);
-                    Display(letterDisplay);
+                    Display(ReturnLetterString(c));
+                    thread_sleep_for(1000);
                 }
                 
         }
@@ -177,11 +177,13 @@ int main()
         if (LastNameButton == false)
         {       
                 executionLCD(2);
-                for (char c : firstName)
+                for (char c : LastName)
                 {
-                    char letterDisplay = ReturnLetterString(c);
-                    Display(letterDisplay);
+                    Display(ReturnLetterString(c));
+                    thread_sleep_for(1000);
                 }
         }
     }
+    
+    
 }
